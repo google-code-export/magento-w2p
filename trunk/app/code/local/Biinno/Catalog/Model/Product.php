764da2b0b2b5e9b7b9944e0642988339fda920c3
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2009-09-28 08:13:38 +0000

    Show all pages previews for multipage templates

--- trunk/app/code/local/Biinno/Catalog/Model/Product.php
+++ trunk/app/code/local/Biinno/Catalog/Model/Product.php
@@ -5,7 +5,10 @@ class Biinno_Catalog_Model_Product extends Mage_Catalog_Model_Product {
     if (!$this->hasData('w2p_image')) return parent::getMediaGalleryImages();
 
     if (!$this->hasData('media_gallery_images') && $this->hasData('w2p_image_links')) {
-      $preview_image_urls = array_slice(explode(',', $this->getData('w2p_image_links')), 1);
+      $preview_image_urls = explode(',', $this->getData('w2p_image_links'));
+
+      if (count($preview_image_urls) == 1) return $this->getData('media_gallery_images');
+
       $images = new Varien_Data_Collection();
 
       foreach ($preview_image_urls as $preview_image_url) {
