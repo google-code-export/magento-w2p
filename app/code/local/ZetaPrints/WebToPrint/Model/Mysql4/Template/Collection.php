b1445609d64539b5ab367f353a29916d24b3b128
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2009-11-16 09:17:17 +0000

    Add new model Template

---
+++ app/code/local/ZetaPrints/WebToPrint/Model/Mysql4/Template/Collection.php
@@ -0,0 +1,9 @@
+<?php
+
+class ZetaPrints_WebToPrint_Model_Mysql4_Template_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract {
+  protected function _construct() {
+    $this->_init('webtoprint/template');
+    }
+}
+
+?>
