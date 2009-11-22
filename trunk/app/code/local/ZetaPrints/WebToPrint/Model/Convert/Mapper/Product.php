a5f69d89fbf93f8cb994df059dab618942014147
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2009-11-22 07:50:09 +0000

    Add products creation

---
+++ trunk/app/code/local/ZetaPrints/WebToPrint/Model/Convert/Mapper/Product.php
@@ -0,0 +1,45 @@
+<?php
+
+class ZetaPrints_WebToPrint_Model_Convert_Mapper_Product extends  Mage_Dataflow_Model_Convert_Mapper_Abstract {
+
+  public function map () {
+    $this->debug = (bool)Mage::getStoreConfig('api/settings/w2p_debug');
+
+    $templates = Mage::getModel('webtoprint/template')->getCollection()->load();
+
+    foreach ($templates as $template) {
+      $product_model = Mage::getModel('catalog/product');
+
+      if ($product_model->getIdBySku($template->getGuid())) {
+        $this->debug("Product {$template->getGuid()} already exists");
+        continue;
+      }
+
+      $this->debug("Product {$template->getGuid()} doesn\'t exists, create it.");
+
+      if (Mage::app()->isSingleStoreMode())
+        $product_model->setWebsiteIds(array(Mage::app()->getStore(true)->getWebsite()->getId()));
+      else
+        $this->debug('Not a single store mode');
+
+      $product_model->setAttributeSetId($product_model->getDefaultAttributeSetId())
+        ->setSku($template->getGuid())
+        ->setTypeId('simple')
+        ->setName($template->getTitle())
+        ->setShortDescription($template->getDescription())
+        ->setVisibility(0)
+        ->save();
+    }
+  }
+
+  private function notice ($message) {
+    $this->addException($message, Mage_Dataflow_Model_Convert_Exception::NOTICE);
+  }
+
+  private function debug ($message) {
+    if ($this->debug)
+      $this->notice($message);
+  }
+}
+
+?>
