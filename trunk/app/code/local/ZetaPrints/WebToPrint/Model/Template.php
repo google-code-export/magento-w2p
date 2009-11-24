021704c9e10cb3b5bfb27e546f13d2ccd170acf2
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2009-11-24 08:39:00 +0000

    Rewrite webtiprint helper for new underlay code

--- trunk/app/code/local/ZetaPrints/WebToPrint/Model/Template.php
+++ trunk/app/code/local/ZetaPrints/WebToPrint/Model/Template.php
@@ -4,6 +4,10 @@ class ZetaPrints_WebToPrint_Model_Template extends Mage_Core_Model_Abstract {
   protected function _construct() {
     $this->_init('webtoprint/template');
   }
+
+  public function loadByGuid ($guid) {
+    return $this->load($this->_getResource()->getIdByGuid($guid));
+  }
 }
 
 ?>
