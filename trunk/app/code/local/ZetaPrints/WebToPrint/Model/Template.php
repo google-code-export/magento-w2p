ce268de9eeff87d06c002b3ec369e6118bcc9c9c
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2009-12-23 07:16:51 +0000

    Fix error in template model

--- trunk/app/code/local/ZetaPrints/WebToPrint/Model/Template.php
+++ trunk/app/code/local/ZetaPrints/WebToPrint/Model/Template.php
@@ -3,10 +3,23 @@
 class ZetaPrints_WebToPrint_Model_Template extends Mage_Core_Model_Abstract {
   protected function _construct() {
     $this->_init('webtoprint/template');
+    $this->setIdFieldName('guid');
   }
 
   public function loadByGuid ($guid) {
     return $this->load($this->_getResource()->getIdByGuid($guid));
+  }
+
+  public function load ($id, $field = 'guid') {
+    return parent::load($id, $field);
+  }
+
+  public function save () {
+    $this->setIdFieldName('template_id');
+    parent::save();
+    $this->setIdFieldName('guid');
+
+    return $this;
   }
 }
 
