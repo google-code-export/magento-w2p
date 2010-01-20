6e7a11e46dbb2dd5644554125ece9d14c809f63c
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-01-20 04:33:30 +0000

    (AccessControl) Add attribution notion

--- app/code/community/ZetaPrints/AccessControl/sql/accesscontrol_setup/mysql4-install-1.php
+++ app/code/community/ZetaPrints/AccessControl/sql/accesscontrol_setup/mysql4-install-1.php
@@ -15,6 +15,7 @@
  * @category   ZetaPrints
  * @package    ZetaPrints_AccessControl
  * @copyright  Copyright (c) 2010 ZetaPrints Ltd. http://www.zetaprints.com/
+ * @attribution Vinai Kopp http://www.magentocommerce.com/extension/reviews/module/635
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @author     Anatoly A. Kazantsev <anatoly.kazantsev@gmail.com>
  */
