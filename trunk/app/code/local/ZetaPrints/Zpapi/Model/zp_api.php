1be1692e1cec1a13476f83a1cf0f44701722d551
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-01-28 09:52:47 +0000

    Fix api page name

--- trunk/app/code/local/ZetaPrints/Zpapi/Model/zp_api.php
+++ trunk/app/code/local/ZetaPrints/Zpapi/Model/zp_api.php
@@ -798,7 +798,7 @@ function zetaprints_get_preview_image_url ($url, $key, $data) {
 function zetaprints_get_user_images ($url, $key, $data) {
   zetaprints_debug();
 
-  $response = zetaprints_get_content_from_url("$url/API.aspx?page=api-img;ApiKey=$key", $data);
+  $response = zetaprints_get_content_from_url("$url/API.aspx?page=api-imgs;ApiKey=$key", $data);
 
   if (zetaprints_has_error($response))
     return null;
