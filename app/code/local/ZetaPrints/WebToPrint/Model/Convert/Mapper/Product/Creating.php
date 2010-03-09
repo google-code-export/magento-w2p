c2aaa3a056d9ffb5e00ac6b2608e2369bbac37bd
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-03-09 10:08:05 +0000

    Generate for product sku cd /home/jimcrow/svn/magento-w2p/trunk/app

--- app/code/local/ZetaPrints/WebToPrint/Model/Convert/Mapper/Product/Creating.php
+++ app/code/local/ZetaPrints/WebToPrint/Model/Convert/Mapper/Product/Creating.php
@@ -1,5 +1,12 @@
 <?php
 
+if (!defined('ZP_API_VER')) {
+  $zetaprints_api_file = Mage::getRoot().'/code/local/ZetaPrints/Zpapi/Model/zp_api.php';
+
+  if (file_exists($zetaprints_api_file))
+    require $zetaprints_api_file;
+}
+
 class ZetaPrints_WebToPrint_Model_Convert_Mapper_Product_Creating extends  Mage_Dataflow_Model_Convert_Mapper_Abstract {
 
   public function map () {
@@ -36,7 +43,7 @@ class ZetaPrints_WebToPrint_Model_Convert_Mapper_Product_Creating extends  Mage_
         $this->debug('Not a single store mode');
 
       $product_model->setAttributeSetId($product_model->getDefaultAttributeSetId())
-        ->setSku("{$template->getGuid()}-rename-me")
+        ->setSku(zetaprints_generate_guid())
         ->setTypeId('simple')
         ->setName($template->getTitle())
         ->setDescription($template->getDescription())
@@ -47,11 +54,12 @@ class ZetaPrints_WebToPrint_Model_Convert_Mapper_Product_Creating extends  Mage_
         ->save();
 
       $stock_item = Mage::getModel('cataloginventory/stock_item');
+
       $stock_item->setStockId(1)
         ->setProduct($product_model)
         ->save();
 
-      $this->debug("Product {$template->getGuid()} was created.");
+      $this->debug("Product for template {$template->getGuid()} was created.");
     }
 
     $this->warning('Warning: products were created with general set of properties. Update other product properties using bulk edit to make them operational.');
