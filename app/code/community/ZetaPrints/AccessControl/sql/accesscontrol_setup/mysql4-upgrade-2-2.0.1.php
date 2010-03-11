7d7193015e5b6f6edcde3153b0021191f2dc933f
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-03-11 06:41:29 +0000

    Reformatting

--- app/code/community/ZetaPrints/AccessControl/sql/accesscontrol_setup/mysql4-upgrade-2-2.0.1.php
+++ app/code/community/ZetaPrints/AccessControl/sql/accesscontrol_setup/mysql4-upgrade-2-2.0.1.php
@@ -3,16 +3,18 @@
 $installer = $this;
 $installer->startSetup();
 
-$entityTypeId     = $installer->getEntityTypeId('catalog_category');
-$attributeSetId   = $installer->getDefaultAttributeSetId($entityTypeId);
-$attributeGroupId = $installer->getDefaultAttributeGroupId($entityTypeId, $attributeSetId);
+$category_entity_id = $installer->getEntityTypeId('catalog_category');
+$category_attribute_set_id = $installer
+                               ->getDefaultAttributeSetId($category_entity_id);
 
 $installer->addAttributeToGroup(
-	$entityTypeId,
-	$attributeSetId,
-	$installer->getAttributeGroupId($entityTypeId, $attributeSetId, 'Display Settings'),
-	'accesscontrol_show_group',
-	60
+  $category_entity_id,
+  $category_attribute_set_id,
+  $installer->getAttributeGroupId($category_entity_id,
+                                  $category_attribute_set_id,
+                                  'Display Settings'),
+  'accesscontrol_show_group',
+  60
 );
 
 $installer->endSetup();
