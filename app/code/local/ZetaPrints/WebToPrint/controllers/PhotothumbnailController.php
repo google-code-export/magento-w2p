74f917f5456166efb4a02bab24d89d105a57da4c
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-05-19 08:37:40 +0000

    Add proper caching dor proxied images

--- app/code/local/ZetaPrints/WebToPrint/controllers/PhotothumbnailController.php
+++ app/code/local/ZetaPrints/WebToPrint/controllers/PhotothumbnailController.php
@@ -15,14 +15,6 @@ class ZetaPrints_WebToPrint_PhotothumbnailController extends Mage_Core_Controlle
 
     $guid = $this->getRequest()->get('guid');
 
-    $type = explode('.', $guid);
-
-    if (count($type) == 2)
-      $type = $type[1];
-
-    if ($type == 'jpg')
-      $type = 'jpeg';
-
     $width = 0;
     if ($this->getRequest()->has('width'))
       $width = (int) $this->getRequest()->get('width');
@@ -39,10 +31,34 @@ class ZetaPrints_WebToPrint_PhotothumbnailController extends Mage_Core_Controlle
 
     $response = zetaprints_get_content_from_url($url);
 
-    if (!zetaprints_has_error($response))
-      $this->getResponse()
-        ->setHeader('Content-Type', 'image/' . $type)
-        ->setBody($response['content']['body']);
+    if (!zetaprints_has_error($response)) {
+      $headers = $response['content']['header'];
+
+      if (is_array($headers))
+        $this->getResponse()
+          ->setHeader('Last-Modified', $headers['Last-Modified'], true)
+          ->setHeader('ETag', $headers['ETag'], true)
+          ->setHeader('Pragma', '', true)
+          ->setHeader('Cache-Control', 'public', true)
+          ->setHeader('Cache-Control', $headers['Cache-Control'])
+          ->setHeader('Expires', '', true)
+          ->setHeader('Content-Type', $headers['Content-Type'] , true)
+          ->setHeader('Content-Length', $headers['Content-Length'], true);
+      else {
+        $type = explode('.', $guid);
+
+        if (count($type) == 2)
+          $type = $type[1];
+
+        if ($type == 'jpg')
+          $type = 'jpeg';
+
+        $this->getResponse()
+          ->setHeader('Content-Type', 'image/' . $type);
+        }
+
+      $this->getResponse()->setBody($response['content']['body']);
+    }
   }
 }
 ?>
--- app/code/local/ZetaPrints/WebToPrint/controllers/PreviewController.php
+++ app/code/local/ZetaPrints/WebToPrint/controllers/PreviewController.php
@@ -93,22 +93,38 @@ class ZetaPrints_WebToPrint_PreviewController extends Mage_Core_Controller_Front
 
     $guid = $this->getRequest()->get('guid');
 
-    $type = explode('.', $guid);
-
-    if (count($type) == 2)
-      $type = $type[1];
-
-    if ($type == 'jpg')
-      $type = 'jpeg';
-
     $url = Mage::getStoreConfig('zpapi/settings/w2p_url') . '/preview/' . $guid;
 
     $response = zetaprints_get_content_from_url($url);
 
-    if (!zetaprints_has_error($response))
-      $this->getResponse()
-        ->setHeader('Content-Type', 'image/' . $type)
-        ->setBody($response['content']['body']);
+    if (!zetaprints_has_error($response)) {
+      $headers = $response['content']['header'];
+
+      if (is_array($headers))
+        $this->getResponse()
+          ->setHeader('Last-Modified', $headers['Last-Modified'], true)
+          ->setHeader('ETag', $headers['ETag'], true)
+          ->setHeader('Pragma', '', true)
+          ->setHeader('Cache-Control', 'public', true)
+          ->setHeader('Cache-Control', $headers['Cache-Control'])
+          ->setHeader('Expires', '', true)
+          ->setHeader('Content-Type', $headers['Content-Type'] , true)
+          ->setHeader('Content-Length', $headers['Content-Length'], true);
+      else {
+        $type = explode('.', $guid);
+
+        if (count($type) == 2)
+          $type = $type[1];
+
+        if ($type == 'jpg')
+          $type = 'jpeg';
+
+        $this->getResponse()
+          ->setHeader('Content-Type', 'image/' . $type);
+        }
+
+      $this->getResponse()->setBody($response['content']['body']);
+    }
   }
 }
 ?>
