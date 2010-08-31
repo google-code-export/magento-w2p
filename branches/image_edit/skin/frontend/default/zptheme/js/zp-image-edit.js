ca4d5acdcc65574851f9a4af2af75070dc7f3b65
igor.snigirev@gmail.com <igor.snigirev@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-08-31 22:21:51 +0000

    Class metadataAccessor is detached in separate file zp-metadata-accessor.js

--- branches/image_edit/skin/frontend/default/zptheme/js/zp-image-edit.js
+++ branches/image_edit/skin/frontend/default/zptheme/js/zp-image-edit.js
@@ -77,50 +77,6 @@ jQuery(document).ready(function ($) {
   }
 
   /**
-   * Class for storing any number of keys and values in specified html input element
-   * @attr: _storageInputElement - html input element
-   */
-  function metadataAccessor (_storageInputElement) {
-    this.metadataAccessor = function(_storageInputElement) {
-      this._storageInputElement = _storageInputElement
-    }
-    this.metadataAccessor(_storageInputElement)
-
-    this.restoreFromStorage = function() {
-      var _metadata = top.userImageThumbSelected.data('metadata');
-      var _key_val_pairs = (_metadata==null) ? [] : _metadata.split(';');
-      // var _key_val_pairs = this._storageInputElement.value.split(';');
-      for (var _i in _key_val_pairs) {
-        [_key, _val] = _key_val_pairs[_i].split('=');
-        this.setProperty(_key, _val);
-      }
-    }
-
-    this.storeAll = function() {
-      var _outArr = [];
-      var _j = 0;
-      for(var _i in this) if (typeof(this[_i])!='function' && _i != '_storageInputElement') {
-        _outArr[_j++] = _i + '=' + this[_i];
-      }
-      var _metadata = _outArr.join(';');
-      top.userImageThumbSelected.data('metadata', _metadata);
-      if ($('input[name=zetaprints-#' + top.image_imageName + ']:checked', top.document).val()==top.userImageThumbSelected.attr('id'))
-        this._storageInputElement.value = _metadata;
-    }
-
-    this.setProperty = function(_propertyName, _propertyValue) {
-      this[_propertyName] = _propertyValue;
-    }
-
-    this.getProperty = function(_propertyName) {
-      if(typeof(this[_propertyName])=='undefined')
-        return null
-      else
-        return this[_propertyName];
-    }
-  }
-
-  /**
    * Fetch stored crop metadata
    */
   function fetchCropMetadata() {
