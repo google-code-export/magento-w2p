464e4cd3530035ff389392be35d77b3e77e0156e
trilobit.alex <trilobit.alex@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-09-09 03:51:16 +0000

    Text field resizing

--- branches/w2p/skin/frontend/default/zptheme/js/resizer.js
+++ branches/w2p/skin/frontend/default/zptheme/js/resizer.js
@@ -1,50 +1,67 @@
-var globID = null; //last resized object
-var handle_path = '/magedev/skin/frontend/default/zptheme/images/handle.gif';
+var glob_id = null; //last resized object
+var glob_x = -1;
 
 jQuery.fn.resizer = function($){
   return this.each(function(){
     var me = $(this);
     var id = me.attr('id');
-    //me.css('height',45);
+    if(me.attr('tagName').toUpperCase()=='TEXTAREA'){
+      me.css('resize','none');
+      me.css('outline-style','none');
+    }
     var h = parseInt(me.css('height'));
     // create container
     me = me.wrap('<div id="wrap_'+id+'" style="position:relative; width: 100%; height:'+h+'px;"></div>');
     var div = $('#wrap_'+id); // get container
     // add a handle
     me.after(
-      $('<div style="cursor:sw-resize; position:relative; background-image:url('+handle_path+'); width:10px; height:10px; top:-10px; -moz-user-select:none; -webkit-user-select:none; user-select:none;" unselectable="on"></div>').bind('mousedown', function(e) {
+      $('<div class="zp-resize-handle" id="handle_'+id+'" unselectable="on"></div>').bind('mousedown', function(e) {
         // onMouseDown
+        $('body').css('-webkit-user-select','none');
         div.css('z-index',1000);
-        globID = me.attr('id');
+        glob_id = me.attr('id');
         var _width = parseInt(me.css('width'));
         var height = parseInt(me.css('height'));
         var x = e.pageX;
         var y = e.pageY;
         var mLeft = parseInt(div.css("margin-left"));
-	// onMouseMove
+	if(glob_x == -1) glob_x = x;
+	if(glob_x > x) glob_x = x;
+
+        // onMouseMove
         var moveHandler = function(e){
           if(me.attr('tagName').toUpperCase()=='TEXTAREA')
             me.css('height',Math.max(h,(e.pageY + height - y)));
-	  if(e.pageX - x + mLeft <0){
-            me.css('width', _width + x - e.pageX);
-            div.css("margin-left", e.pageX - x + mLeft);
-	  }
+          if($(document.getElementById('handle_'+id+'')).css('left')=='-4px'){
+            if( e.pageX >= glob_x)
+              div.css('width', _width + e.pageX - x);
+          }else{
+            if(e.pageX - x + mLeft <0){
+              me.css('width', _width + x - e.pageX);
+              div.css("margin-left", e.pageX - x + mLeft);
+	    }
+          }
         };
 	// onMouseUp
-        var upHandler = function(e){ $(document).unbind('mousemove',moveHandler).unbind('mouseup',upHandler); me.focus();};
+        var upHandler = function(e){
+          $(document).unbind('mousemove',moveHandler).unbind('mouseup',upHandler);
+          me.focus();
+          $('body').css('-webkit-user-select','auto');
+        };
 	// bind listeners
         $(document).bind('mousemove', moveHandler).bind('mouseup', upHandler);
       })
     );
     me.focus(function () {
-      globID = null;
+      glob_id = null;
     });
     // reset object on blur
     me.blur(function () {
-      if(me.attr('id')!=globID){
+      if(me.attr('id')!=glob_id){
         if(me.attr('tagName').toUpperCase()=='TEXTAREA')
           me.css('height','');
         me.css('width','');
+        div.css('width','');
         div.css("margin-left", '');
         div.css('z-index','');
       }
