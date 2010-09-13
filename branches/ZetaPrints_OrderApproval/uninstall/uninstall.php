6fa9f13185df56edf98a52bf2c1d2103296eda10
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-09-13 17:51:17 +0000

    (OrderApproval) Add uninstall script

---
+++ branches/ZetaPrints_OrderApproval/uninstall/uninstall.php
@@ -0,0 +1,40 @@
+<?php
+
+//Initialize Magento
+require_once 'app/Mage.php';
+umask(0);
+
+//Run Mage app
+Mage::app('default');
+
+//Get setup model
+$setup = Mage::getModel('eav/entity_setup',  'core');
+
+$setup->startSetup();
+
+echo 'Removing attributes ';
+
+//Remove installed attributes
+$setup->removeAttribute('customer', 'is_approver');
+$setup->removeAttribute('customer', 'approver');
+
+echo '[OK]<br />';
+
+echo 'Cleaning tables ';
+
+//Remove approved column from quote items
+$setup->run("
+  ALTER TABLE {$setup->getTable('sales/quote_item')} DROP COLUMN `approved`");
+
+//Remove record about extension from resource table
+$setup->run("
+  DELETE FROM {$setup->getTable('core/resource')}
+    WHERE code = 'orderapproval_setup'");
+
+echo '[OK]<br />';
+
+$setup->endSetup();
+
+echo 'ZetaPrints OrderApproval extension was completely removed';
+
+?>
