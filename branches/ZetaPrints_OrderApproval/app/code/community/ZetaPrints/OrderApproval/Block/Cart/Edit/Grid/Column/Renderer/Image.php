47308df996ad5333aafdcc0277b7440e0f965757
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-09-20 11:44:36 +0000

    Fix error with dublicated previews in approval UI. Issue #302 c. 15

--- branches/ZetaPrints_OrderApproval/app/code/community/ZetaPrints/OrderApproval/Block/Cart/Edit/Grid/Column/Renderer/Image.php
+++ branches/ZetaPrints_OrderApproval/app/code/community/ZetaPrints/OrderApproval/Block/Cart/Edit/Grid/Column/Renderer/Image.php
@@ -1,9 +1,7 @@
 <?php
 
 class ZetaPrints_OrderApproval_Block_Cart_Edit_Grid_Column_Renderer_Image
-  extends ZetaPrints_OrderApproval_Block_Widget_Grid_Column_Renderer_Image {
-
-  protected $_image_data = null;
+  extends ZetaPrints_OrderApproval_Block_Cart_Edit_Grid_Column_Renderer_Abstract {
 
   public function __construct() {
     parent::__construct();
@@ -11,27 +9,12 @@ class ZetaPrints_OrderApproval_Block_Cart_Edit_Grid_Column_Renderer_Image
     $this->setTemplate('checkout/cart/edit/grid/column/renderer/image.phtml');
   }
 
-  public function render (Varien_Object $row) {
-    if (is_null($this->_image_data))
-      $this->_image_data = parent::_getValue($row);
-
-    if (!isset($this->_image_data['item'])) {
-      return '';
-    }
-
-    return $this->_toHtml();
+  public function getProductThumbnail () {
+    return $this->getItemRendererBlock()->getProductThumbnail();
   }
 
-  public function getImageUrl () {
-    return $this->_image_data['url'];
-  }
-
-  public function getAltText () {
-    return $this->_image_data['alt'];
-  }
-
-  public function getItem () {
-    return $this->_image_data['item'];
+  public function getProductName () {
+    return $this->getItemRendererBlock()->getProductName();
   }
 }
 
