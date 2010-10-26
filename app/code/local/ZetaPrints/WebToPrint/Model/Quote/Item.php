6bf956a2abebade53b3ed6c9f86fb026dabd39f1
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-10-26 16:07:11 +0000

    Do not allow merge of web-to-print products while checkout (Issue #334)

--- app/code/local/ZetaPrints/WebToPrint/Model/Quote/Item.php
+++ app/code/local/ZetaPrints/WebToPrint/Model/Quote/Item.php
@@ -7,6 +7,21 @@ class ZetaPrints_WebToPrint_Model_Quote_Item extends Mage_Sales_Model_Quote_Item
 
     return parent::representProduct($product);
   }
+
+  public function compare ($item) {
+    //Get model for info_buyRequest option
+    $option_model = $item->getOptionByCode('info_buyRequest');
+
+    //Unserialize its value
+    $options = unserialize($option_model->getValue());
+
+    //Check if quote item represents web-to-print product.
+    //If it does then do not merge these items.
+    if (isset($options['zetaprints-TemplateID']))
+      return false;
+
+    return parent::compare($item);
+  }
 }
 
 ?>
