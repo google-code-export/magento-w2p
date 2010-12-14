b5142ffd0d96866af788d8747f680299498f7e46
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2010-12-14 20:43:17 +0000

    Implement infobar (Issue #360); dummy initial positioning

--- trunk/skin/frontend/default/zptheme/js/zp-image-edit.js
+++ trunk/skin/frontend/default/zptheme/js/zp-image-edit.js
@@ -10,49 +10,89 @@ jQuery(document).ready(function ($) {
               .parents('td')
               .children('input.zetaprints-images');
 
+  var $info_bar = $('div.info-bar');
   var $restore_button = $('#restore-button');
 
   var crop_data = null;
 
+  var info_bar_elements = {
+    'frame': {
+      'width': $('#frame-width'),
+      'height': $('#frame-height'),
+      'dpi': $('#frame-dpi') },
+
+    'uploaded': {
+      'width': $('#uploaded-width'),
+      'height': $('#uploaded-height'),
+      'dpi': $('#uploaded-dpi') },
+
+    'recommended': {
+      'width': $('#recommended-width'),
+      'height': $('#recommended-height'),
+      'dpi': $('#recommended-dpi') } };
+
+  set_info_bar_value('recommended', 'width', _cropVisualAssistant.getPlaceholderInfo().widthPx);
+  set_info_bar_value('recommended', 'height', _cropVisualAssistant.getPlaceholderInfo().heightPx);
+  set_info_bar_value('recommended', 'dpi', _cropVisualAssistant.getPlaceholderInfo().resolution);
+
+  var image_to_shape_factor = null;
+
   function imageEditorCrop () {
-    showImageEditorTooltip('Select visible part');
+    set_info_bar_state();
 
     var cropMetadata = _cropVisualAssistant.getInitCroppedArea(0, 0);
 
+    var container_width = $('#userImagePreviewContainer').width();
+    var container_height = $('#userImagePreviewContainer').height();
+
+    var shape_width = _cropVisualAssistant.getPlaceholderInfo().widthPx;
+    var shape_height = _cropVisualAssistant.getPlaceholderInfo().heightPx;
+
+    var width_factor = shape_width / container_width;
+    var height_factor = shape_height / container_height;
+
+    var factor = width_factor > height_factor ? width_factor : height_factor;
+
+    var selection_width = Math.round(shape_width / factor);
+    var selection_height = Math.round(shape_height / factor);
+
+    var image_width = Math.round(_cropVisualAssistant.userImage.widthActualPx / factor);
+    var image_height = Math.round(_cropVisualAssistant.userImage.heightActualPx / factor);
+
     var data = {
       selection: {
-        width: cropMetadata[2] - cropMetadata[0],
-        height: cropMetadata[3] - cropMetadata[1],
+        width: selection_width,
+        height: selection_height,
         position: {
-          top: cropMetadata[1],
-          left: cropMetadata[0] } } };
+          top: 0,
+          left: 0 } },
+      image: {
+        width: image_width,
+        height: image_height,
+        position: {
+          top: 0,
+          left: 0 } } };
 
     var metadata = $input.data('metadata');
 
     if (isCropFit && metadata) {
-      var width = Number($('#userImagePreview').width());
-      var height = Number($('#userImagePreview').height());
-
       var cr_x1 = metadata['cr-x1'];
       var cr_y1 = metadata['cr-y1'];
       var cr_x2 = metadata['cr-x2'];
       var cr_y2 = metadata['cr-y2'];
 
-
       if (cr_x1 && cr_y1 && cr_x2 && cr_y2)
         data.selection = {
-          width: (cr_x2 - cr_x1) * width,
-          height: (cr_y2 - cr_y1) * height,
+          width: (cr_x2 - cr_x1) * data.selection.width,
+          height: (cr_y2 - cr_y1) * data.selection.height,
           position: {
-            top: cr_y1 * height,
-            left: cr_x1 * width } };
+            top: cr_y1 * data.selection.width,
+            left: cr_x1 * data.selection.height } };
 
       var selection_position = data.selection.position;
       var selection_size = {
         width: data.selection.width,
         height: data.selection.height };
-
-      data.image = {};
 
       var sh_x = metadata['sh-x'];
       var sh_y = metadata['sh-y'];
@@ -83,13 +123,13 @@ jQuery(document).ready(function ($) {
   function imageEditorHideCrop() {
     $('#userImagePreview').power_crop('destroy');
 
-    $('#imageEditorTooltip').hide();
+    //$('#imageEditorTooltip').hide();
 
     $('div.zetaprints-buttons-row').hide();
 
     $('#imageEditorCropForm').hide();
 
-    $('#imageEditorImageInfo').empty();
+    //$('#imageEditorImageInfo').empty();
   }
 
   function imageEditorUpdateCropCoords (data) {
@@ -118,9 +158,29 @@ jQuery(document).ready(function ($) {
       var width = Math.round(image_size.width * _cropVisualAssistant.getPlaceholderInfo().widthPx / c.w);
       var height = Math.round(image_size.height * _cropVisualAssistant.getPlaceholderInfo().heightPx / c.h);
 
-      updateEditAndSaveInfoBar(width, height);
+      set_info_bar_value('frame', 'width', width);
+      set_info_bar_value('frame', 'height', height);
 
-      _cropVisualAssistant.updateInfoBar(width, height);
+      var thumb_width = _cropVisualAssistant.userImage.widthPreviewPx;
+
+      if (_cropVisualAssistant.getPlaceholderInfo().clipped == true ||
+          width > _cropVisualAssistant.templateImage.widthPx ||
+          height > _cropVisualAssistant.templateImage.heightPx) {
+
+        var factor = data.selection.width / image_size.width;
+        var dpi = Math.round(_cropVisualAssistant.getPlaceholderInfo().resolution * factor);
+
+        if (dpi < _cropVisualAssistant.getPlaceholderInfo().resolution)
+          set_info_bar_state('low-resolution-warning');
+        else
+          set_info_bar_state();
+
+      } else {
+        var dpi =  _cropVisualAssistant.getPlaceholderInfo().resolution;
+        set_info_bar_state();
+      }
+
+      set_info_bar_value('frame', 'dpi', dpi);
 
       var image_position = data.image.position;
 
@@ -155,8 +215,8 @@ jQuery(document).ready(function ($) {
 
       _cropVisualAssistant.updateView([c.x, c.y, c.x2, c.y2]);
     } else {
-      updateEditAndSaveInfoBar(c.w, c.h);
-      _cropVisualAssistant.updateInfoBar(c.w, c.h);
+      //updateEditAndSaveInfoBar(c.w, c.h);
+      //_cropVisualAssistant.updateInfoBar(c.w, c.h);
     }
 
     setTimeout(imageEditorAdjustSize, 100);
@@ -238,7 +298,7 @@ jQuery(document).ready(function ($) {
       },
       success: function (data, textStatus) {
         imageEditorApplyImage(data);
-        showImageEditorTooltip('Image Cropped');
+        //showImageEditorTooltip('Image Cropped');
         $('#userImagePreview').fadeIn();
       }
     });
@@ -260,7 +320,7 @@ jQuery(document).ready(function ($) {
     },
     success: function (data, textStatus) {
       imageEditorApplyImage(data);
-      showImageEditorTooltip('Image Restored');
+      //showImageEditorTooltip('Image Restored');
       $('#userImagePreview').fadeIn();
     }
     });
@@ -281,7 +341,7 @@ jQuery(document).ready(function ($) {
         },
       success: function (data, textStatus) {
         imageEditorApplyImage(data);
-        showImageEditorTooltip('Image Loaded');
+        //showImageEditorTooltip('Image Loaded');
       }
     });
   }
@@ -302,7 +362,7 @@ jQuery(document).ready(function ($) {
       },
       success: function (data, textStatus) {
         imageEditorApplyImage(data);
-        showImageEditorTooltip('Image Rotated');
+        //showImageEditorTooltip('Image Rotated');
         $('#userImagePreview').fadeIn();
       }
     });
@@ -316,7 +376,7 @@ jQuery(document).ready(function ($) {
     var userImageWidthActual, userImageHeightActual, uh, uw, src;
 
     $('#userImagePreview').attr("src", "");
-    $('#imageEditorInfoBar').hide();
+
     parent.jQuery.fancybox.showActivity();
 
     userImageSrc = editor_image_url_template.replace('image-guid.image-ext', getRegexpValue(xml, /Thumb="([^"]*?)"/));
@@ -344,13 +404,18 @@ jQuery(document).ready(function ($) {
       return false;
     } else {
       _cropVisualAssistant.setUserImage($('#userImagePreview'), userImageWidthActual, userImageHeightActual, userImageWidthPreview, userImageHeightPreview);
+
+      set_info_bar_value('uploaded', 'width', userImageWidthActual);
+      set_info_bar_value('uploaded', 'height', userImageHeightActual);
     }
+
+    image_to_shape_factor = _cropVisualAssistant.templateImage.widthPx / userImageWidthActual;
 
     $('#userImagePreview').attr("src", userImageSrc);
     $('#userImagePreview').width(userImageWidthPreview);
     $('#userImagePreview').height(userImageHeightPreview);
 
-    updateEditAndSaveInfoBar(userImageWidthPreview, userImageHeightPreview);
+    //updateEditAndSaveInfoBar(userImageWidthPreview, userImageHeightPreview);
 
     tmp1 = $('input[value=' + imageEditorId + ']', top.document).parent().find('img');
     if (tmp1.length == 0)
@@ -388,48 +453,13 @@ jQuery(document).ready(function ($) {
   }
 
   /**
-   * Update InfoBar
-   */
-  function updateEditAndSaveInfoBar(_width, _height)
-  {
-    $('#imageEditorWidthInfo').html(_width + ' px');
-    $('#imageEditorHeightInfo').html(_height + ' px');
-  }
-
-  /**
-   * Get InfoBar for "Edit And Save" fancybox
-   */
-  function getEditAndSaveInfoBar()
-  {
-    return $(
-      '<TABLE id="infoBar"><TR>' +
-      '<TD>' + zetaprints_trans('W:') + '</TD><TD id="imageEditorWidthInfo"></TD>' +
-      '<TD>' + zetaprints_trans('H:') + '</TD><TD id="imageEditorHeightInfo"></TD>' +
-      '</TR></TABLE>');
-  }
-
-  /**
-   * Show messages in the InfoBar imageEditorTooltip
-   */
-  function showImageEditorTooltip(_message)
-  {
-    $('#imageEditorInfoBar').show();
-    // $('#imageEditorCropForm').css('display', 'block');
-
-    $('#imageEditorTooltip').html(zetaprints_trans(_message));
-    $('#imageEditorTooltip').show('fast', function () {
-      imageEditorAdjustSize();
-    });
-  }
-
-  /**
    * Adjust fancybox size, place it in center of browser window,
    * and place user image in the center of the fancybox
    */
   function imageEditorAdjustSize()
   {
-    var userImagePreviewWidth = $('#userImagePreview').width();
-    var userImagePreviewHeight = $('#userImagePreview').height();
+    var userImagePreviewWidth = 600; //$('#userImagePreview').width();
+    var userImagePreviewHeight = 450; //$('#userImagePreview').height();
 
     var infoBarWidth = $('#imageEditorBottomPanel').width();
     var infoBarHeight = $('#imageEditorBottomPanel').height() + 10;
@@ -475,6 +505,21 @@ jQuery(document).ready(function ($) {
     parent.jQuery.fancybox.center();
   }
 
+  function set_info_message (text) {
+   $('#info-message').html(zetaprints_trans(text));
+  }
+
+  function set_info_bar_value (type, key, value) {
+    info_bar_elements[type][key].html(value);
+  }
+
+  function set_info_bar_state (warning) {
+    if (warning)
+      $info_bar.addClass('warning ' + warning);
+    else
+      $info_bar.removeClass('warning low-resolution-warning');
+  }
+
   // Check if zetaprints_trans function exists, if not exists create dummy one
   if (!window.zetaprints_trans) {
     function zetaprints_trans (msg) {
@@ -508,18 +553,18 @@ jQuery(document).ready(function ($) {
         _cropVisualAssistant.cropedAreaShow();
       });
 
-      $('#imageEditorImageInfo').empty().append(getEditAndSaveInfoBar());
-      updateEditAndSaveInfoBar(userImageWidthPreview, userImageHeightPreview);
+      //$('#imageEditorImageInfo').empty().append(getEditAndSaveInfoBar());
+      //updateEditAndSaveInfoBar(userImageWidthPreview, userImageHeightPreview);
     } else {
       imageEditorCrop();
 
-      _cropVisualAssistant.getInfoBar().appendTo($('#imageEditorImageInfo'));
-      _cropVisualAssistant.updateInfoBar(userImageWidthPreview, userImageHeightPreview);
+      //_cropVisualAssistant.getInfoBar().appendTo($('#imageEditorImageInfo'));
+      //_cropVisualAssistant.updateInfoBar(userImageWidthPreview, userImageHeightPreview);
     }
 
     $('#userImagePreview').ready(function () {
       parent.jQuery('#fancybox-loading').fadeOut();
-      $('#imageEditorInfoBar').show();
+      //$('#imageEditorInfoBar').show();
     });
 
     imageEditorAdjustSize();
@@ -561,4 +606,29 @@ jQuery(document).ready(function ($) {
     imageEditorDoRotate('l');
   });
   $('#imageEditorDelete').click(imageEditorDelete);
+
+  $('div.info-link').click(function () {
+    var $bar = $(this).parents('div.info-bar');
+
+    if ($bar.hasClass('opened'))
+      $bar.removeClass('opened');
+    else
+      $bar.addClass('opened');
+  });
+
+  $('div.notice-switchers div.icon', $info_bar).hover(
+    function () {
+      $(this).addClass('hover');
+
+      var $notice_container = $('div.notices div.'
+                                + $(this).attr('rel'), $info_bar);
+
+      $notice_container.addClass('notice');
+    },
+
+    function () {
+      $(this).removeClass('hover');
+      $('div.notices div.notice').removeClass('notice');
+    }
+  );
 });
