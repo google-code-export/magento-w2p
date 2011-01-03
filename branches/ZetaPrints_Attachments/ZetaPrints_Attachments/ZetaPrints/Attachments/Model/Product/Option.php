266197ebebf3d91949f39760aabba704f0ad496c
jamby77@gmail.com <jamby77@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-01-03 11:45:55 +0000

    Fixed bug about issue #406 Changed version number Some formatting changes

--- branches/ZetaPrints_Attachments/ZetaPrints_Attachments/ZetaPrints/Attachments/Model/Product/Option.php
+++ branches/ZetaPrints_Attachments/ZetaPrints_Attachments/ZetaPrints/Attachments/Model/Product/Option.php
@@ -35,7 +35,7 @@ class ZetaPrints_Attachments_Model_Product_Option extends Mage_Catalog_Model_Pro
         $product = Mage::getModel('catalog/product')->load($this->getData('product_id'));
       }
       if ($product instanceof Mage_Catalog_Model_Product ){
-        if($product->getAttributeText('use_ajax_upload') != 'Yes') {
+        if(!Mage::helper('attachments/upload')->getUseAjax($product)) {
           return parent::getGroupByType($type);
         }
       }
