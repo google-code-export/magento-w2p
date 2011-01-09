9c8995da738c6a3249b3d04aec2d27ea84e1f968
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-01-09 19:38:22 +0000

    (Fixedprices) Make code style more consistent. Make code a bit simple and easier for reading. Wrap lines to 80 symbols

--- branches/ZetaPrints_Fixedprices/ZetaPrints_Fixedprices/ZetaPrints/Fixedprices/Model/Events/Observers/Product/Collection.php
+++ branches/ZetaPrints_Fixedprices/ZetaPrints_Fixedprices/ZetaPrints/Fixedprices/Model/Events/Observers/Product/Collection.php
@@ -1,6 +1,6 @@
 <?php
-class ZetaPrints_Fixedprices_Model_Events_Observers_Product_Collection
-{
+class ZetaPrints_Fixedprices_Model_Events_Observers_Product_Collection {
+
   /**
    * Update some of the prices in product collection
    *
@@ -9,22 +9,24 @@ class ZetaPrints_Fixedprices_Model_Events_Observers_Product_Collection
    *
    * @param Varien_Event_Observer $observer
    */
-  public function updatePrices(Varien_Event_Observer $observer){
+  public function updatePrices (Varien_Event_Observer $observer) {
     $collection = $observer->getCollection();
-    /*@var $collection Mage_Catalog_Model_Resource_Eav_Mysql4_Product_Collection */
-      foreach ($collection as $item) {
-        /* @var $item Mage_Catalog_Model_Product */
-        if(Mage::helper('fixedprices')->isFixedPriceEnabled($item)){
-          $price = $item->getPrice();
-          if($price != $item->getData('price')){ // if price returned from method is different than raw 'price' data
-            // then we probably have fixed price
-            $fixedPrice = $item->getFixedPrice();
-            if($fixedPrice){
-              $item->setMinPrice($price)->setMinimalPrice($price)->setMaxPrice($price)->setFinalPrice($price);
-            }
-          }
-        }
-      }
-  }
 
+    /*@var $collection Mage_Catalog_Model_Resource_Eav_Mysql4_Product_Collection */
+    foreach ($collection as $item) {
+      /* @var $item Mage_Catalog_Model_Product */
+      if (!Mage::helper('fixedprices')->isFixedPriceEnabled($item))
+        continue;
+
+      $price = $item->getPrice();
+
+      // if price returned from method is different than raw 'price' data
+      // and we have fixed price
+      if ($price != $item->getData('price') && $item->getFixedPrice())
+        $item->setMinPrice($price)
+          ->setMinimalPrice($price)
+          ->setMaxPrice($price)
+          ->setFinalPrice($price);
+    }
+  }
 }
