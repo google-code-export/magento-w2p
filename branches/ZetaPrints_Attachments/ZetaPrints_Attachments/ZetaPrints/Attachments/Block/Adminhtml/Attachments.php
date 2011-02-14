88819cbff54e6dda6bacce85e9359c0281aad912
jamby77@gmail.com <jamby77@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-02-14 14:47:58 +0000

    Added table listing attachments, with delete action per row and mass delete action.

--- branches/ZetaPrints_Attachments/ZetaPrints_Attachments/ZetaPrints/Attachments/Block/Adminhtml/Attachments.php
+++ branches/ZetaPrints_Attachments/ZetaPrints_Attachments/ZetaPrints/Attachments/Block/Adminhtml/Attachments.php
@@ -6,14 +6,23 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class ZetaPrints_Attachments_Block_Adminhtml_Attachment extends Mage_Adminhtml_Block_Widget_Grid_Container
+class ZetaPrints_Attachments_Block_Adminhtml_Attachments extends Mage_Adminhtml_Block_Widget_Grid_Container
 {
   public function __construct()
   {
     $this->_controller = 'adminhtml_attachments';
     $this->_blockGroup = 'attachments';
-    $this->_headerText = Mage::helper('attachments')->__('Item Manager');
-    $this->_addButtonLabel = Mage::helper('attachments')->__('Add Item');
+    $this->_headerText = Mage::helper('attachments')->__('Attachments Manager');
     parent::__construct();
+
+    $this->removeButton('add');
+    $this->_addButton('deleteall', array ('label' => 'Delete All',
+                                    'onclick' => 'setLocation(\'' . $this->getUrl('*/*/deleteAll') . '\')',
+                                    'class' => 'delete'
+    ));
+    $this->_addButton('deleteorphaned', array ('label' => 'Delete Orphaned',
+                                    'onclick' => 'setLocation(\'' . $this->getUrl('*/*/deleteOrphaned') . '\')',
+                                    'class' => 'delete'
+    ));
   }
 }
---
+++ branches/ZetaPrints_Attachments/ZetaPrints_Attachments/ZetaPrints/Attachments/controllers/Adminhtml/AttachmentsController.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * @author 			Petar Dzhambazov
+ * @category    ZetaPrints
+ * @package     ZetaPrints_Attachments
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class ZetaPrints_Attachments_Adminhtml_AttachmentsController
+ extends Mage_Adminhtml_Controller_Action
+{
+  protected $images = array (
+    'image/jpeg',
+    'image/pjpeg',
+    'image/gif',
+  	'image/png',
+  	'image/x-ms-bmp',
+  	'image/x-bmp'
+  );
+
+  protected function _initAction($menu = 'attachments/items')
+  {
+    $this->loadLayout()
+          ->_setActiveMenu($menu)
+          ->_addBreadcrumb(Mage::helper('adminhtml')->__('Attachments Manager'),
+                           Mage::helper('adminhtml')->__('Attachments Manager'));
+
+    return $this;
+  }
+
+  public function indexAction()
+  {
+    $this->_initAction()
+         ->renderLayout();
+  }
+
+  public function deleteAction()
+  {
+    if ($this->getRequest()->getParam('attachment_id') > 0) {
+      try {
+        $model = Mage::getModel('attachments/attachments');
+        /* @var $model ZetaPrints_Attachments_Model_Attachments */
+        $model->load($this->getRequest()->getParam('attachment_id'))->deleteFile();
+
+        Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')
+                                                ->__('Attachment was successfully deleted'));
+        $this->_redirect('*/*/');
+      } catch (Exception $e) {
+        Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+        $this->_redirect('*/*/edit', array ('id' => $this->getRequest()->getParam('id')));
+      }
+    }
+    $this->_redirect('*/*/');
+  }
+
+  public function massDeleteAction()
+  {
+    $attachmentsIds = $this->getRequest()->getParam('attachments');
+    if (!is_array($attachmentsIds)) {
+      Mage::getSingleton('adminhtml/session')->addError(Mage::helper('adminhtml')
+                                             ->__('Please select attachment(s)'));
+    } else {
+      try {
+        foreach ($attachmentsIds as $attachmentsId) {
+          $attachments = Mage::getModel('attachments/attachments')->load($attachmentsId);
+          /* @var $attachments ZetaPrints_Attachments_Model_Attachments */
+          $attachments->deleteFile();
+        }
+        Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')
+                                               ->__('Total of %d record(s) were successfully deleted', count($attachmentsIds)));
+      } catch (Exception $e) {
+        Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+      }
+    }
+    $this->_redirect('*/*/index');
+  }
+
+  public function downloadAction()
+  {
+    $attachmentId = $this->getRequest()->getParam('id');
+    $hash = $this->getRequest()->getParam('key');
+    $att = Mage::getModel('attachments/attachments')->load($attachmentId);
+    if ($att) {
+      try {
+        $value = unserialize($att->getAttachmentValue());
+        if ($hash != $value['secret_key']) {
+          $this->_forward('noRoute');
+        }
+
+        $filePath = Mage::getBaseDir() . $value['order_path'];
+        if (!is_file($filePath) || !is_readable($filePath)) {
+          // try get file from quote
+          $filePath = Mage::getBaseDir() . $value['quote_path'];
+          if (!is_file($filePath) || !is_readable($filePath)) {
+            throw new Exception();
+          }
+        }
+
+        $disposition = 'attachment'; // set default file disposition to attachment
+        if (in_array($value['type'], $this->images)) { // if we have image file, change it to inline
+          $disposition = 'inline';
+          if ($value['type'] == 'image/pjpeg')
+            $value['type'] = 'image/jpeg'; // if file has been uploaded via IE, set correct jpeg header
+        }
+
+        $this->getResponse()
+             ->setHttpResponseCode(200)
+             ->setHeader('Pragma', 'public', true)
+             ->setHeader('Cache-Control', 'must-revalidate, post-check=0, pre-check=0', true)
+             ->setHeader('Content-type', $value['type'], true)
+             ->setHeader('Content-Length', $value['size'])
+             ->setHeader('Content-Disposition', $disposition . '; filename="' . $value['title'] . '"');
+
+        $this->getResponse()->clearBody();
+        $this->getResponse()->sendHeaders();
+
+        readfile($filePath);
+      } catch (Exception $e) {
+        $this->_forward('noRoute');
+      }
+    } else {
+      $this->_forward('noRoute');
+    }
+  }
+}
