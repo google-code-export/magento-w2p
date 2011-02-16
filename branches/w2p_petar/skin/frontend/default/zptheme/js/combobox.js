e840c08985a8cf697f3a7c2616909b151cb3e12f
jamby77@gmail.com <jamby77@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-02-16 19:32:45 +0000

    Added combobox functionality for lists that contain '-' option item. This is achieved using latest jQuery ui library. Because of that I have added some more content to jquery-ui-1.8.7-custom-min.js Styling is made using ui utilities as well, that is why I have added base theme from jquery ui. Concrete combobox implementation is in combobox.js and some minor styling is in combobox.css

---
+++ branches/w2p_petar/skin/frontend/default/zptheme/js/combobox.js
@@ -0,0 +1,131 @@
+(function($){
+    $.widget("ui.combobox", {
+        _create: function(){
+            var self = this, 
+                select = this.element.hide(), 
+                selected = select.children(":selected"), 
+                value = selected.val() ? selected.text() : "";
+            var input = $("<input>")
+                .insertAfter(select)
+                .val(value)
+                .autocomplete({
+                    delay: 0,
+                    minLength: 0,
+                    source: function(request, response){
+                        var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
+                        response(select.children("option").map(function(){
+                            var text = $(this).text();
+                            if (this.value && (!request.term || matcher.test(text))) 
+                                return {
+                                    label: text.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" +
+                                    $.ui.autocomplete.escapeRegex(request.term) +
+                                    ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>"),
+                                    value: text,
+                                    option: this
+                                };
+                        }));
+                    },
+                    select: function(event, ui){
+                        ui.item.option.selected = true;
+                        self._trigger("selected", event, {
+                            item: ui.item.option
+                        });
+                    },
+                    change: function(event, ui){
+                        if (!ui.item) {
+                            var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex($(this).val()) + "$", "i"), valid = false;
+                            select.children("option").each(function(){
+                                if ($(this).text().match(matcher)) {
+                                    this.selected = valid = true;
+                                    return false;
+                                }
+                            });
+                            if (!valid) {
+                                // remove invalid value, as it didn't match anything
+                                // above line is from original implementation, 
+                                // we however want to add the value to the list instead
+                                // and select it. 
+                                var val = $(this).val();
+                                select.append('<option>' + val + '</option>');
+                                select.val(val);
+                                input.data("autocomplete").term = val;
+                            }
+                        }
+                    }
+            }).addClass("ui-widget ui-widget-content ui-corner-left");
+            
+            this.input = input;
+            
+            input.data("autocomplete")._renderItem = function(ul, item){
+                return $("<li></li>").data("item.autocomplete", item).append("<a>" + item.label + "</a>").appendTo(ul);
+            };
+            
+            // repeating the check from above because when clicking on a submit button
+            // right after entering new value change event does not fire
+            input.blur(function(event){
+                var self = jQuery(this);
+                var val = self.val();
+                
+                var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(val) + "$", "i"), valid = false;
+                select.children("option").each(function(){
+                    if ($(this).text().match(matcher)) {
+                        this.selected = valid = true;
+                        return false;
+                    }
+                });
+                if (!valid) {
+                    // remove invalid value, as it didn't match anything
+                    // above line is from original implementation, 
+                    // we however want to add the value to the list instead
+                    // and select it. 
+                    select.append('<option>' + val + '</option>');
+                    select.val(val);
+                    input.data("autocomplete").term = val;
+                }
+            });
+            
+            this.button = $("<button type='button'>&nbsp;</button>").attr("tabIndex", -1).attr("title", "Show All Items").insertAfter(input).button({
+                icons: {
+                    primary: "ui-icon-triangle-1-s"
+                },
+                text: false
+            }).removeClass("ui-corner-all").addClass("ui-corner-right ui-button-icon").click(function(){
+                // close if already visible
+                if (input.autocomplete("widget").is(":visible")) {
+                    input.autocomplete("close");
+                    return;
+                }
+                
+                // pass empty string as value to search for, displaying all results
+                input.autocomplete("search", "");
+                input.focus();
+            });
+        },
+        
+        destroy: function(){
+            this.input.remove();
+            this.button.remove();
+            this.element.show();
+            $.Widget.prototype.destroy.call(this);
+        }
+    });
+})(jQuery);
+
+jQuery(function(){
+    var $selects = jQuery('select');
+    $selects.each(function(){
+        var $self = jQuery(this);
+        var isCombo = false;
+        var $children = $self.children('option');
+        $children.each(function(){
+            var $opt = jQuery(this);
+            if (jQuery.trim($opt.text()) == '-') {
+                isCombo = true;
+                $opt.remove();
+            }
+        });
+        if (isCombo) {
+            $self.wrap('<div class="ui-widget"/>').combobox();
+        }
+    });
+});
