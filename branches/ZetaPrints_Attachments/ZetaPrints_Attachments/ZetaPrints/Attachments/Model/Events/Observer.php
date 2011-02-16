ed2a846b345b68cf12689f5c4599747b2d63142a
jamby77@gmail.com <jamby77@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-02-16 08:20:02 +0000

    Added auto delete functionality using crontab options in Magento

--- branches/ZetaPrints_Attachments/ZetaPrints_Attachments/ZetaPrints/Attachments/Model/Events/Observer.php
+++ branches/ZetaPrints_Attachments/ZetaPrints_Attachments/ZetaPrints/Attachments/Model/Events/Observer.php
@@ -158,5 +158,128 @@ class ZetaPrints_Attachments_Model_Events_Observer
                                               && Mage::helper('attachments/upload')->getUseAjax($product);
     return $return;
   }
+
+  /**
+   * Delete old and obsolete files
+   *
+   * Not all files that are uploaded on product page
+   * end up being part of an order. These files are
+   * obsolete and only fill up hard disk.
+   * Also after certain period of time, you have completed
+   * your orders and thus files attached to them are of no
+   * use besides taking up space.
+   * We are handling these files as 2 cases - files that do not
+   * belong to an order (orphaned) and old files (these are files that have
+   * been uploaded some period ago regardless if they are part of an order or not)
+   * This method is used to delete both of those file cases.
+   * For it to function correctly you have to set periods for either
+   * option in System > Configuration > Attachments Options > Settings
+   * If any of the periods is 0 or empty ot non integer value,
+   * it will be ignored and that type of files will not be deleted.
+   * By default the settings are to delete obsolete files after 30 days
+   * and not to delete old files at all.
+   * Bare in mind that Old files include both orphaned files and files used in
+   * orders, so if you specify value for old files that is equal or less
+   * than the value for orphaned files, the latter is simply ignored since ALL files
+   * will be deleted by the former setting anyway.
+   * 
+   * This is run using Magento cron tab. Recomennded setting for cron tab
+   * in production environment is: * /5 * * * * /path/to/php /path/to/magento/cron.php >/dev/null 2>&1
+   * for testing and debugging purposes last portion of the line 
+   * could be changed to a log file:
+   * * /5 * * * * /path/to/php /path/to/magento/cron.php >>/path/to/magento/var/log/cron.log
+   * This way all output from the operation will be logged.
+   * 
+   * @return void
+   */
+  public function cleanUpOldFiles()
+  {
+    $baseNode = 'attachments/settings/';
+    $oldFiles = $baseNode . 'att_old_days';
+    $orphanFiles = $baseNode . 'att_orphan_days';
+    $oldNode = Mage::getStoreConfig($oldFiles);
+    $orphanNode = Mage::getStoreConfig($orphanFiles);
+    $this->_debug('Old files period: ' . $oldNode);
+    $this->_debug('Orphan files period: ' . $orphanNode);
+
+    // var_dump($oldNode, $orphanNode);
+    if($oldNode && is_numeric($oldNode) && $oldNode > 0 ||
+          $orphanNode && is_numeric($orphanNode) && $orphanNode > 0){ // if any of the periods is set
+
+
+      $model = Mage::getModel('attachments/attachments');
+
+      $collection = $model->getCollection();
+      $collection->addFieldToSelect('*');
+      $select = $collection->getSelect();
+
+      $this->_setConditions($oldNode, $orphanNode, $select);
+
+      $this->_debug('Query executed' . $select);
+
+      $collection->load();
+      $this->_debug(count($collection) . ' files will be deleted');
+      try{
+        $counter = 0;
+        foreach($collection as $item){
+          // $item->deleteFile();
+          $counter++;
+        }
+        $this->_debug($counter . ' items deleted');
+      }catch(Exception $e){
+        $this->_debug($e->getMessage());
+      }
+    }else{
+      $this->_debug('No files found');
+    }
+  }
+
+  /**
+   * Add appropriate condition to selection
+   *
+   * @param  integer $old - Old files period
+   * @param  integer $orphaned - Orphaned files period
+   * @param Varien_Db_Select $select - DB Select Object
+   * @return Varien_Db_Select
+   */
+  protected function _setConditions($old, $orphaned, Varien_Db_Select $select)
+  {
+    $periodField = ZetaPrints_Attachments_Model_Attachments::ATT_CREATED; // table field 'created'
+    $orderField = ZetaPrints_Attachments_Model_Attachments::ORD_ID; // table field 'order_id'
+    $orphanedWhere = "DATE_SUB(CURDATE(),INTERVAL ? DAY) > `{$periodField}` AND `{$orderField}` IS NULL"; // orphaned condition
+    $oldWhere = "DATE_SUB(CURDATE(),INTERVAL ? DAY) > `{$periodField}`"; // old condition
+
+    if ($old && $orphaned) { // if both periods are greater than 0 - add both conditions
+
+      if($orphaned < $old){  // if $orphaned period is sooner then $old, add them both
+        $select->where($orphanedWhere, $orphaned)
+              ->orWhere($oldWhere, $old);
+      }else{ // else add just old
+        $select->where($oldWhere, $old);
+      }
+
+    } else if ($orphaned) { // else add which ever is set
+      $select->where($orphanedWhere, $orphaned);
+    } else {
+      $select->where($oldWhere, $old);
+    }
+
+    return $select;
+  }
+  
+  /**
+   * Echo debug value
+   * 
+   * Only echo in developer mode.
+   */
+  protected function _debug($value)
+  {      
+    echo date('r') . ' [DEBUG]: ' . $value . PHP_EOL;
+    /*
+    if (isset($_SERVER['MAGE_IS_DEVELOPER_MODE'])) {
+      echo date('r') . ' [DEBUG]: ' . $value . PHP_EOL;
+    }
+    */
+  }
 }
 
