71ba8f1a3df174e81244dc25b2d720e449b5b7f5
jamby77@gmail.com <jamby77@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-03-11 15:11:55 +0000

    Implemented line breaks in comments. On frontend only customer comments have shaded background Tried to hide actual comments before they are processed by JavaScript, it will still have a little flick, but that is all I can do without modifying template files. Admin comments do not loos their look after comment is posted.

--- js/custom/ordercomments.js
+++ js/custom/ordercomments.js
@@ -2,18 +2,22 @@ if (typeof Prototype != 'undefined') { // check that Prototype is loaded
   Event.observe(window, 'load', function(e) {
     var commentsStack = $$('.order-comments dl.order-about');
     if (commentsStack.length != 0) {
-
-      var comments = commentsStack[0].select('dd');
+      var dl = commentsStack[0];
+      dl.addClassName('hide');
+      var comments = dl.select('dd');
       comments.each(function(comm) {
         var cont = comm.innerHTML;
+        var br = /&lt;br\s*?\/?&gt;/;
+        cont = cont.gsub(br, '<br/>');
         if (cont.endsWith('...customer')) {
           var l = cont.length - '...customer'.length;
           cont = cont.substr(0, l);
-          comm.addClassName('customer-comment').update(cont).insert({top: '<strong>You:</strong> '});
+          comm.addClassName('customer-comment').update(cont).insert({top: '<strong>You:</strong><br/>'});
         }else{
-          comm.insert({top: '<strong>Admin:</strong> '});
+          comm.insert({top: '<strong>Admin:</strong><br/>'});
         }
       });
+      dl.removeClassName('hide');
     }
     var backLink = $$('div.order-details div.buttons-set');
     if (backLink.length) {
