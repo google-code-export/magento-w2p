c4ce1259e2e47f1a67366ec2e3bb4680ab7488e9
jamby77@gmail.com <jamby77@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-03-15 10:04:50 +0000

    Clean up a bit

--- branches/ZetaPrints_Fixedprices/ZetaPrints_Fixedprices/ZetaPrints/Fixedprices/Model/Events/Observers/Fixedprices.php
+++ branches/ZetaPrints_Fixedprices/ZetaPrints_Fixedprices/ZetaPrints/Fixedprices/Model/Events/Observers/Fixedprices.php
@@ -5,11 +5,10 @@ class ZetaPrints_Fixedprices_Model_Events_Observers_Fixedprices
   const COOKIE_NAME = 'fp_items';
   public function getItemId($observer){
     $quote_items = $observer->getEvent()->getCart()->getItems(); // get all items
-    $cookie = '';
     $ids = array();
 
     foreach ($quote_items as $quote_item){ // loop them
-      /*@var $quote_item Mage_Sales_Model_Quote_Item */
+      /* @var $quote_item Mage_Sales_Model_Quote_Item */
       $product = $quote_item->getProduct();
       if ($product->getData(ZetaPrints_Fixedprices_Helper_Data::FIXED_PRICE)){ // if item product uses FQ
         $id = $quote_item->getId(); // get quote item id - it should be unique for each product you add
@@ -22,10 +21,6 @@ class ZetaPrints_Fixedprices_Model_Events_Observers_Fixedprices
     } // end foreach
 
     $cookie = implode(',', $ids);
-//    $path = rtrim(Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_DIRECT_LINK), '/');
-//    $fullpath = Mage::getUrl('checkout/cart');
-//    $path = str_ireplace($path, '', $fullpath);
-//    setcookie(self::COOKIE_NAME, $cookie, 0, $path);
     $session = Mage::getSingleton('checkout/session');
     /* @var $session Mage_Checkout_Model_Session */
     $session->setData(self::COOKIE_NAME, $cookie);
