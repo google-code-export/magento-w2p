2dd213a65b668a0819faee6ec0b82cf50c77cfbd
jamby77@gmail.com <jamby77@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-04-06 16:58:15 +0000

    DistributionMap.Changed admin setting for zoom level to use drop down list. Set some meaningful etiquettes.

---
+++ branches/ZetaPrints_DistributionMap/app/code/community/ZetaPrints/DistributionMap/Model/Source/Zoom.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: pp
+ * Date: 4/6/11
+ * Time: 7:36 PM
+ */
+
+class ZetaPrints_DistributionMap_Model_Source_Zoom
+{
+  /**
+   * Return formatted zoom levels
+   *       1 => 'Entire earth',
+      3 => 'Continent - North America',
+      4 => 'Big country - US, China, Russia, Australia',
+      8 => 'State',
+      10 => 'Big city',
+      12 => 'Town, suburb',
+      15 => 'Neighbourhood',
+      18 => 'Street block',
+      20 => 'Individual building',
+   * @return array
+   */
+  public function toOptionArray()
+  {
+    $zoom_levels = range(0, 20, 1);
+    $labels = array(
+      1 => 'Entire earth',
+      4 => 'Big country - US, China, Russia, Australia',
+      8 => 'State',
+      12 => 'Town, suburb',
+      15 => 'Neighbourhood',
+      18 => 'Street block',
+      20 => 'Individual building',
+    );
+    foreach($zoom_levels as $key => $val) {
+      if(isset($labels[$key])) {
+        $zoom_levels[$key] = $val . ' - ' . $labels[$key];
+      }else {
+        $zoom_levels[$key] = (string)$val;
+      }
+    }
+    return $zoom_levels;
+  }
+}
