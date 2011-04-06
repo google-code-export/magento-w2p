8e47df7f1e1078b9b77709a09b5695c7fa1c52aa
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-04-06 06:45:34 +0000

    Merge translations into a sing cd /home/jimcrow/svn/magento-w2p/trunk/apple file

--- app/code/local/ZetaPrints/WebToPrint/Model/Events/Observer.php
+++ app/code/local/ZetaPrints/WebToPrint/Model/Events/Observer.php
@@ -321,6 +321,76 @@ class ZetaPrints_WebToPrint_Model_Events_Observer implements ZetaPrints_Api {
       $customer->setZetaprintsPassword($credentials['password']);
     }
   }
+
+  public function complete_zetaprints_order ($observer) {
+    $order = $observer->getEvent()->getOrder();
+
+    foreach ($order->getAllItems() as $item) {
+      $options = $item->getProductOptions();
+
+      if (!isset($options['info_buyRequest']['zetaprints-order-id']))
+        continue;
+
+      //If the item was reordered skip it (we can't complete already completed
+      //order on ZetaPrints)
+      if (isset($options['info_buyRequest']['zetaprints-reordered'])
+          && $options['info_buyRequest']['zetaprints-reordered'] === true)
+        continue;
+
+      $url = Mage::getStoreConfig('zpapi/settings/w2p_url');
+      $key = Mage::getStoreConfig('zpapi/settings/w2p_key');
+
+      //GUID for ZetaPrints order which was saved on Add to cart step
+      $current_order_id = $options['info_buyRequest']['zetaprints-order-id'];
+      //New GUID for completed order
+      $new_order_id = zetaprints_generate_guid();
+
+      $order_details = zetaprints_complete_order($url, $key, $current_order_id,
+                                                                 $new_order_id);
+
+      if (!$order_details) {
+        //Check if saved order exists on ZetaPrints...
+        if (zetaprints_get_order_details($url, $key, $current_order_id)) {
+          //... then try again to complete the order
+          $order_details = zetaprints_complete_order($url, $key,
+                                              $current_order_id, $new_order_id);
+
+          //If it fails...
+          if (!$order_details) {
+            //... then set state for order in M. as problems and add comment
+            $order->setState('problems', true,
+                'Use the link to ZP order to troubleshoot.')
+              ->save();
+            return;
+          }
+        }
+        //... otherwise try to get order details by new GUID and if completed
+        //order doesn't exist in ZetaPrints...
+        else if (!$order_details =
+                       zetaprints_get_order_details($url, $key, $new_order_id)) {
+          //... then set state for order in M. as problems and add comment about
+          //failed order on ZetaPrints side.
+          $order->setState('problems', true,
+                  'Failed order. Contact admin@zetaprints.com ASAP to resolve.')
+            ->save();
+
+          return;
+        }
+      }
+
+      $types = array('pdf', 'gif', 'png', 'jpeg', 'cdr');
+
+      foreach ($types as $type)
+        if (strlen($order_details[$type]))
+          $options['info_buyRequest']['zetaprints-file-'.$type] = $url . '/' . $order_details[$type];
+
+      $options['info_buyRequest']['zetaprints-order-id'] = $order_details['guid'];
+
+      $item->setProductOptions($options)->save();
+    }
+
+    return $this;
+  }
 }
 
 ?>
