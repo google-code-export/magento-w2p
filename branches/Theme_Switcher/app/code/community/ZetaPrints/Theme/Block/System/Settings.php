d8f49924bb18c5cac3aea31520af582c437569b2
jamby77@gmail.com <jamby77@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-04-15 11:04:26 +0000

    Theme switcher added to repository.

---
+++ branches/Theme_Switcher/app/code/community/ZetaPrints/Theme/Block/System/Settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: pp
+ * Date: 4/7/11
+ * Time: 5:37 PM
+ */
+
+class ZetaPrints_Theme_Block_System_Settings
+  extends ZetaPrints_Theme_Block_Abstract
+{
+
+}
