65fa54ccd43baa50c1880b334e42c371235d1654
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-04-25 09:55:42 +0000

    Add additional info messages (issue #527)

--- trunk/app/code/community/ZetaPrints/WebToPrint/Model/Convert/Parser/Template.php
+++ trunk/app/code/community/ZetaPrints/WebToPrint/Model/Convert/Parser/Template.php
@@ -42,39 +42,58 @@ class ZetaPrints_WebToPrint_Model_Convert_Parser_Template
       return;
     }
 
+    $total_number_of_catalogs = 0;
+    $number_of_ignored_catalogs = 0;
+
     $total_number_of_templates = 0;
     $number_of_added_templates = 0;
     $number_of_uptodate_templates = 0;
     $number_of_updated_templates = 0;
     $number_of_removed_templates = 0;
+    $number_of_ignored_templates = 0;
+
+    $line = 0;
 
     $all_template_guids = array();
 
     foreach ($catalogs as $catalog) {
+      $total_number_of_catalogs++;
+
       $templates = zetaprints_get_templates_from_catalog($url, $key, $catalog['guid']);
 
       if ($templates === null) {
         $this->error("Error in parsing templates detailes xml from catalog {$catalog['title']}");
+
+        $number_of_ignored_catalogs++;
+
         continue;
       } else if (is_string($templates)) {
         $this->error("Error in receiving list of templates for catalog {$catalog['title']}: {$templates}");
+
+        $number_of_ignored_catalogs++;
+
         continue;
       }
 
       if (!count($templates)) {
         $this->warning("No templates in catalog {$catalog['title']}");
+
+        $number_of_ignored_catalogs++;
+
         continue;
       }
 
       $total_number_of_templates += count($templates);
 
       foreach ($templates as $template) {
+        $line++;
+
         $all_template_guids[$template['guid']] = $template['guid'];
 
         $template_array = zetaprints_get_template_detailes($url, $key, $template['guid']);
 
         if (!$template_array) {
-          $this->error("Error in receiving or parsing detailes of template {$template['guid']}. Leaving the template unmodified.");
+          $this->error("{$line}. Error in receiving or parsing detailes of template {$template['guid']}. Leaving the template unmodified.");
           continue;
         }
 
@@ -98,19 +117,19 @@ class ZetaPrints_WebToPrint_Model_Convert_Parser_Template
               $template_model->setExist(false)->save();
               $number_of_removed_templates++;
 
-              $this->warning("Template {$template['guid']} has no variable fields. Template will be deleted.");
+              $this->warning("{$line}. Template {$template['guid']} has no variable fields. Template will be deleted.");
               continue;
             }
 
             if (strtotime($template['date']) > strtotime($template_model->getDate())
              || $refresh_templates
              || (int)$template_model->getPublic() != $template['public']) {
-              $this->debug("Template {$template['guid']} is outdated");
+              $this->debug("{$line}. Template {$template['guid']} is outdated");
 
               $template['xml'] = zetaprints_get_template_details_as_xml($url, $key, $template['guid']);
 
               if (!$template['xml']) {
-                $this->error("Error in receiving detailes for template {$template['guid']}. Leaving the template unmodified.");
+                $this->error("{$line}. Error in receiving detailes for template {$template['guid']}. Leaving the template unmodified.");
                 continue;
               }
 
@@ -119,28 +138,36 @@ class ZetaPrints_WebToPrint_Model_Convert_Parser_Template
             }
             else {
               $number_of_uptodate_templates++;
-              $this->debug("Template {$template['guid']} is up to date");
+              $this->debug("{$line}. Template {$template['guid']} is up to date");
             }
           }
         else {
           if (!$has_fields) {
-            $this->warning("Template {$template['guid']} has no variable fields. Ignored.");
+            $this->warning("{$line}. Template {$template['guid']} has no variable fields. Ignored.");
+
+            $number_of_ignored_templates++;
+
             continue;
           }
 
           $template['xml'] = zetaprints_get_template_details_as_xml($url, $key, $template['guid']);
 
           if (!$template['xml']) {
-            $this->error("Error in receiving detailes for template {$template['guid']}. Passing the template.");
+            $this->error("{$line}. Error in receiving detailes for template {$template['guid']}. Passing the template.");
+
+            $number_of_ignored_templates++;
+
             continue;
           }
 
           $template_model = Mage::getModel('webtoprint/template');
           $template_model->addData($template)->save();
 
-          if ($template_model->getId())
+          if ($template_model->getId()) {
             $number_of_added_templates++;
-            $this->notice("Template {$template['guid']} is succesfully added.");
+
+            $this->notice("{$line}. Template {$template['guid']} is succesfully added.");
+          }
         }
       }
     }
@@ -156,11 +183,18 @@ class ZetaPrints_WebToPrint_Model_Convert_Parser_Template
         $template->setExist(false)->save();
       }
 
+    $this->notice("Total number of catalogs: {$total_number_of_catalogs}");
+    $this->notice("Number of ignored catalogs: {$number_of_ignored_catalogs}");
+
     $this->notice("Total number of templates: {$total_number_of_templates}");
     $this->notice("Number of added templates: {$number_of_added_templates}");
     $this->notice("Number of up to date templates: {$number_of_uptodate_templates}");
     $this->notice("Number of updated templates: {$number_of_updated_templates}");
+    $this->notice("Number of ignored templates: {$number_of_ignored_templates}");
     $this->notice("Number of removed templates: {$number_of_removed_templates}");
+
+    if ($number_of_added_templates > 0)
+      $this->notice('Run ZetaPrints product creation profile to create products for new templates automatically. Read <a href="http://www.zetaprints.com/magentohelp/">www.zetaprints.com/magentohelp/</a> for more info on product creation.');
   }
 
   public function unparse() {}
