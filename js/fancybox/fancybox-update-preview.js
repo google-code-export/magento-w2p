db3b1c8e238bd8b2b5b4ac827a3affe489fc3ab6
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2011-06-10 14:08:57 +0000

    Show rotating arrows in fancybox while updating preview (issue #582)

--- js/fancybox/fancybox-update-preview.js
+++ js/fancybox/fancybox-update-preview.js
@@ -2,7 +2,9 @@ function fancybox_add_update_preview_button ($, zp) {
   var $outer = $('#fancybox-outer');
 
   var $update_preview = $('<a id="zp-update-preview-button">' +
-                            '<span class="icon"/>' +
+                            '<span class="icon left-part">' +
+                              '<span class="icon arrows" />' +
+                            '</span>' +
                             '<span class="text">' +
                               '<span>' + update_preview_button_text + '</span>' +
                             '</span>' +
@@ -18,13 +20,25 @@ function fancybox_add_update_preview_button ($, zp) {
   }
 
   $update_preview.click(function () {
-    if ($outer.hasClass('modified'))
-      zp.update_preview({ data: { zp: zp } });
+    if (!$outer.hasClass('modified'))
+      return false;
+
+    $outer.find('#fancybox-img').bind('load.update-preview', function (event) {
+      $(this).unbind('load.update-preview');
+
+       $outer.removeClass('preview-updating');
+    });
+
+    $outer.addClass('preview-updating');
+
+    zp.update_preview({ data: { zp: zp } });
   })
 }
 
 function fancybox_remove_update_preview_button ($) {
   $('#zp-update-preview-button').remove();
   $('#fancybox-resize').removeClass('middle-position');
+  $('#fancybox-outer').removeClass('preview-updating');
+  $('#fancybox-img').unbind('load.update-preview');
 }
 
