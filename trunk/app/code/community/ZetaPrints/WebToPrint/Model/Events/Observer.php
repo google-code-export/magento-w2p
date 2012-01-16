9f080b2065f35375fcc648f3726b1883bd25d30f
Anatoly.Kazantsev <Anatoly.Kazantsev@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2012-01-16 19:25:16 +0000

    Add support for M <= 1.6.0.0; bug happens only on product page

--- trunk/app/code/community/ZetaPrints/WebToPrint/Model/Events/Observer.php
+++ trunk/app/code/community/ZetaPrints/WebToPrint/Model/Events/Observer.php
@@ -496,7 +496,7 @@ class ZetaPrints_WebToPrint_Model_Events_Observer implements ZetaPrints_Api {
   public function saveOrderId ($observer) {
     $params = $observer->getEvent()->getParams();
 
-    if ($params->getConfigureMode()) {
+    if ($params && s$params->getConfigureMode()) {
       $buyRequest = $params->getBuyRequest();
 
       Mage::register('webtoprint-order-id', $buyRequest['zetaprints-order-id']);
