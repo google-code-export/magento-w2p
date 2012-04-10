0c48eb373302de0d4a30853d357801fabd373125
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2012-04-10 07:23:54 +0000

    (Moxi) Add attributes for admin part (issue #779)

---
+++ branches/ZetaPrints_Moxi/app/code/community/ZetaPrints/Moxi/sql/moxi_setup/install-1.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Catalog
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+$setName = 'OpenX Advertising Plan';
+$groupName = 'OpenX';
+
+$entityTypeId = Mage::getModel('catalog/product')
+                  ->getResource()
+                  ->getTypeId();
+
+$defaultSetId = $this->getDefaultAttributeSetId($entityTypeId);
+
+$set = Mage::getModel('eav/entity_attribute_set')
+         ->setEntityTypeId($entityTypeId)
+         ->setAttributeSetName($setName);
+
+$set->validate();
+
+$set
+  ->save()
+  ->initFromSkeleton($defaultSetId)
+  ->save();
+
+$setId = $set->getId();
+
+$this->addAttributeGroup($entityTypeId, $setId, $groupName);
+$groupId = $this->getAttributeGroupId($entityTypeId, $setId, $groupName);
+
+$siteAttr = array(
+  //Global settings
+  'type' => 'int',
+  'input' => null,
+  'label' => 'OpenX Website ID',
+  'required' => false,
+  'user_defined' => true,
+  'default' => 0,
+  'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+
+  //Catalogue setting
+  'visible' => false,
+  'is_configurable' => false
+);
+
+$this->addAttribute($entityTypeId, 'openx_website_id', $siteAttr);
+$siteAttrId = $this->getAttributeId($entityTypeId, 'openx_website_id');
+
+$zoneAttr = array(
+  //Global settings
+  'type' => 'int',
+  'input' => null,
+  'label' => 'OpenX Zone ID',
+  'required' => false,
+  'user_defined' => true,
+  'default' => 0,
+  'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+
+  //Catalogue setting
+  'visible' => false,
+  'is_configurable' => false
+);
+
+$this->addAttribute($entityTypeId, 'openx_zone_id', $zoneAttr);
+$zoneAttrId = $this->getAttributeId($entityTypeId, 'openx_zone_id');
+
+$this->addAttributeToSet($entityTypeId, $setId, $groupId, $siteAttrId);
+$this->addAttributeToSet($entityTypeId, $setId, $groupId, $zoneAttrId);
+
+$this->updateAttribute($entityTypeId, $siteAttrId, 'is_user_defined', 0);
+$this->updateAttribute($entityTypeId, $zoneAttrId, 'is_user_defined', 0);
