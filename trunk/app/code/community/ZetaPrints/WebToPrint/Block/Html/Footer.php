478db43916135409c81a29cbca8b58fcd063e30a
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2012-04-11 03:29:29 +0000

    Bump version up to 2.1.1.0

--- trunk/app/code/community/ZetaPrints/WebToPrint/Block/Html/Footer.php
+++ trunk/app/code/community/ZetaPrints/WebToPrint/Block/Html/Footer.php
@@ -3,7 +3,7 @@
 class ZetaPrints_WebToPrint_Block_Html_Footer extends Mage_Page_Block_Html_Footer {
   public function getCopyright() {
     return parent::getCopyright()
-      . '<br /><span id="zetaprints-extension-version"><a href="http://www.zetaprints.com/">Web-to-print and image generation</a>, v. 2.1.0.0</span>';
+      . '<br /><span id="zetaprints-extension-version"><a href="http://www.zetaprints.com/">Web-to-print and image generation</a>, v. 2.1.1.0</span>';
   }
 }
 
