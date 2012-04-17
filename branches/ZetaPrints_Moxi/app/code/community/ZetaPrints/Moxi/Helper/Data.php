4ff377c3d0a4b3c9105fc792fb7e095a849a8005
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2012-04-17 17:59:18 +0000

    Add OpenX API wrapper for frontend functionality

--- branches/ZetaPrints_Moxi/app/code/community/ZetaPrints/Moxi/Helper/Data.php
+++ branches/ZetaPrints_Moxi/app/code/community/ZetaPrints/Moxi/Helper/Data.php
@@ -60,6 +60,25 @@ class ZetaPrints_Moxi_Helper_Data extends Mage_Core_Helper_Abstract {
     return $sites;
   }
 
+  public function getZone ($zoneId) {
+    $api = $this->_getApi();
+
+    $zone = $api->getZone($zoneId);
+
+    return new Varien_Object(array('id' => $zone->zoneId,
+                                   'site' => $zone->publisherId,
+                                   'name' => $zone->zoneName,
+                                   'type' => $zone->type,
+                                   'width' => $zone->width,
+                                   'height' => $zone->height,
+                                   'capping' => $zone->capping,
+                                   'session_capping' => $zone->sessionCapping,
+                                   'block' => $zone->block,
+                                   'comments' => $zone->comments,
+                                   'append' => $zone->append,
+                                   'prepend' => $zone->prepend ));
+  }
+
   public function getZonesBySite ($site) {
     $api = $this->_getApi();
 
@@ -85,5 +104,125 @@ class ZetaPrints_Moxi_Helper_Data extends Mage_Core_Helper_Abstract {
                           'prepend' => $zone->prepend )));
 
     return $zones;
+  }
+
+  public function getCampaignsByAdvertiser ($advertiser) {
+    $api = $this->_getApi();
+
+    $id = $advertiser->getId();
+
+    $campaigns = new Varien_Data_Collection();
+
+    foreach ($api->getCampaignListByAdvertiserId($id) as $campaign)
+      $campaigns
+        ->addItem(
+          new Varien_Object(
+            array('id' => $campaign->campaignId,
+                  'advertiser' => $campaign->advertiserId == $id
+                                    ? $advertiser : $campaign->advertiserId,
+                  'name' => $campaign->campaignName,
+                  'begin' => $campaign->startDate,
+                  'end' => $campaign->endDate,
+                  'impressions' => $campaign->impressions,
+                  'clicks' => $campaign->clicks,
+                  'priority' => $campaign->priority,
+                  'weight' => $campaign->weight,
+                  'target_impressions' => $campaign->targetImpressions,
+                  'target_clicks' => $campaign->targetClicks,
+                  'target_conversions' => $campaign->targetConversions,
+                  'revenue' => $campaign->revenue,
+                  'revenue_type' => $campaign->revenueType,
+                  'capping' => $campaign->capping,
+                  'session_capping' => $campaign->sessionCapping,
+                  'block' => $campaign->block,
+                  'comments' => $campaign->comments )));
+
+    return $campaigns;
+  }
+
+  public function addAdvertiser ($name, $email) {
+    $api = $this->_getApi();
+
+    $managerId = (int) Mage::getStoreConfig('moxi/settings/manager');
+
+    $advertiser = new OA_Dll_AdvertiserInfo();
+
+    $advertiser->advertiserName = $name;
+    $advertiser->contactName = $name;
+    $advertiser->emailAddress = $email;
+    $advertiser->agencyId = $managerId;
+
+    $result = $api->addAdvertiser($advertiser);
+
+    if (!$result)
+      return false;
+
+    return new Varien_Object(array('id' => $result,
+                                   'manager' => $managerId,
+                                   'name' => $name,
+                                   'contact_name' => $name,
+                                   'email' => $email,
+                                   'comments' => '' ));
+  }
+
+  public function addCampaign ($advertiser, $name, $begin, $end) {
+    $api = $this->_getApi();
+
+    $campaign = new OA_Dll_CampaignInfo();
+
+    $name = "{$advertiser->getName()} - {$name}";
+    $begin = new DateTime($begin);
+    $end = new DateTime($end);
+
+    $campaign->advertiserId = $advertiser->getId();
+    $campaign->campaignName = $name;
+    $campaign->startDate = $begin;
+    $campaign->endDate = $end;
+
+    $result = $api->addCampaign($campaign);
+
+    if (!$result)
+      return false;
+
+    return new Varien_Object(array('id' => $result,
+                                   'advertiser' => $advertiser,
+                                   'name' => $name,
+                                   'begin' => $begin,
+                                   'end' => $end ));
+  }
+
+  public function addBanner ($campaign, $name, $url, $content) {
+    $api = $this->_getApi();
+
+    $banner = new OA_Dll_BannerInfo();
+
+    $banner->campaignId = $campaign->getId();
+    $banner->bannerName = $name;
+    $banner->storageType = 'web';
+    $banner->url = $url;
+    $banner->aImage = array('filename' => $name, 'content' => $content);
+
+    $result = $api->addBanner($banner);
+
+    if (!$result)
+      return false;
+
+    return new Varien_Object(array('id' => $result,
+                                   'campaign' => $campaign,
+                                   'name' => $name,
+                                   'storage_type' => 'web',
+                                   'url' => $url ));
+  }
+
+  public function linkBannerToZone ($banner, $zone) {
+    $api = $this->_getApi();
+
+    return $api->linkBanner($zone->getId(), $banner->getId());
+  }
+
+  public function linkCampaignToZone ($campaign, $zone) {
+    $api = $this->_getApi();
+
+    return $api->linkCampaign($zone->getId(), $campaign->getId());
   }
 }
