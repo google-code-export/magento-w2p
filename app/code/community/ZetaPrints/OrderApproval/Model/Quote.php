f2dfdf657e6a194cee4b1d868907e88e093d5a98
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2013-01-08 13:14:38 +0000

    (OrderApproval) Implement new logic in Quote class (issue #813)

--- app/code/community/ZetaPrints/OrderApproval/Model/Quote.php
+++ app/code/community/ZetaPrints/OrderApproval/Model/Quote.php
@@ -2,296 +2,56 @@
 
 class ZetaPrints_OrderApproval_Model_Quote extends Mage_Sales_Model_Quote {
 
-  protected $_approved_items = null;
+  /**
+   * Load only approved items and store them as items_collection to use them
+   * int the quote.
+   * 
+   * getItemsCollection() method returns items_collection if it's set, otherwise
+   * it returns $_items field.
+   *
+   * @return Mage_Sales_Model_Quote
+   */
+  public function useOnlyApprovedItems () {
 
-  protected function _afterSave () {
+    //Save quote items collection if it was loaded previously to store
+    //possible changes in quote items before loading collection with approved
+    //items only
     if ($this->_items !== null)
-      $this->getAllItemsCollection()->save();
-    else if ($this->_approved_items !== null)
-      $this->getApprovedItemsCollection()->save();
+      $this
+        ->getItemsCollection()
+        ->save();
 
-    $_items = $this->_items;
-    $this->_items = null;
+    $items = Mage::getModel('sales/quote_item')
+               ->getCollection()
+               ->addFieldToFilter('approved', 1)
+               ->setQuote($this);
 
-    parent::_afterSave();
-
-    $this->_items = $_items;
-  }
-
-  public function getAllItemsCollection () {
-    return parent::getItemsCollection();
-  }
-
-  public function getApprovedItemsCollection () {
-     if (is_null($this->_approved_items)) {
-      $this->_approved_items = Mage::getModel('sales/quote_item')
-        ->getCollection()
-        ->addFieldToFilter('approved', 1)
-        ->setQuote($this);
-    }
-
-    return $this->_approved_items;
-  }
-
-  public function getItemsCollection($use_cache = true) {
-    return $this->getApprovedItemsCollection();
-  }
-
-  public function getAllItemsCount () {
-    $number = 0;
-
-    foreach ($this->getAllItemsCollection() as $item) {
-      if ($item->isDeleted() || $item->getParentItemId()
-          || $item->getParentItem())
-        continue;
-
-      $number++;
-    }
-
-    return $number;
-  }
-
-  public function getAllItems ($incl_unapproved = false) {
-    if (!$incl_unapproved)
-      return parent::getAllItems();
-
-    $items = array();
-
-    foreach ($this->getAllItemsCollection() as $item)
-      if (!$item->isDeleted())
-        $items[] =  $item;
-
-    return $items;
-  }
-
-  public function hasItems ($incl_unapproved = false) {
-    if (!$incl_unapproved)
-      return parent::hasItems();
-
-    return sizeof($this->getAllItems(true)) > 0;
-  }
-
-  public function getItemById ($itemId, $incl_unapproved = false) {
-    if (!$incl_unapproved)
-      return parent::getItemById($itemId);
-
-    return $this->getAllItemsCollection()->getItemById($itemId);
-  }
-
-  public function removeItem ($itemId, $incl_unapproved = false) {
-    if (!$incl_unapproved)
-      return parent::removeItem($itemId);
-
-    $item = $this->getItemById($itemId, true);
-
-    if ($item) {
-      $item->setQuote($this);
-
-      //If we remove item from quote - we can't use multishipping mode
-      $this->setIsMultiShipping(false);
-      $item->isDeleted(true);
-
-      if ($item->getHasChildren())
-        foreach ($item->getChildren() as $child)
-          $child->isDeleted(true);
-
-      Mage::dispatchEvent('sales_quote_remove_item', array('quote_item' => $item));
-    }
-
-    return $this;
-  }
-
-  public function getAllVisibleItems ($incl_unapproved = false) {
-    if (!$incl_unapproved)
-      return parent::getAllVisibleItems();
-
-    $items = array();
-
-    foreach ($this->getAllItemsCollection() as $item)
-      if (!$item->isDeleted() && !$item->getParentItemId())
-        $items[] =  $item;
-
-    return $items;
-  }
-
-  public function addItem (Mage_Sales_Model_Quote_Item $item) {
-    // Work around for M. versions < 1.4.1.0
-    if (method_exists($item, "isNominal"))
-      if ($item->isNominal() && $this->hasItems() || $this->hasNominalItems())
-        Mage::throwException(Mage::helper('sales')
-          ->__('Nominal item can be purchased standalone only. To proceed please remove other items from the quote.'));
-
-    $item->setQuote($this);
-
-    if (!$item->getId()) {
-      $this->getAllItemsCollection()->addItem($item);
-
-      Mage::dispatchEvent('sales_quote_add_item', array('quote_item' => $item));
-    }
-
-    return $this;
-  }
-
-  public function getItemByProduct ($product) {
-    foreach ($this->getAllItems(true) as $item)
-      if ($item->representProduct($product))
-        return $item;
-
-    return false;
-  }
-
-  public function collectTotals() {
-    if ($this->getTotalsCollectedFlag())
-      return $this;
-
-    Mage::dispatchEvent($this->_eventPrefix . '_collect_totals_before',
-                                          array($this->_eventObject => $this));
-
-    $this->setSubtotal(0);
-    $this->setBaseSubtotal(0);
-
-    $this->setSubtotalWithDiscount(0);
-    $this->setBaseSubtotalWithDiscount(0);
-
-    $this->setGrandTotal(0);
-    $this->setBaseGrandTotal(0);
-
-    foreach ($this->getAllAddresses() as $address) {
-      $address->setSubtotal(0);
-      $address->setBaseSubtotal(0);
-
-      $address->setGrandTotal(0);
-      $address->setBaseGrandTotal(0);
-
-      $address->collectTotals();
-
-      $this->setSubtotal((float) $this->getSubtotal()+$address->getSubtotal());
-      $this->setBaseSubtotal((float) $this->getBaseSubtotal()
-                                                + $address->getBaseSubtotal() );
-
-      $this->setSubtotalWithDiscount((float) $this->getSubtotalWithDiscount()
-                                        + $address->getSubtotalWithDiscount() );
-      $this->setBaseSubtotalWithDiscount(
-                                  (float) $this->getBaseSubtotalWithDiscount()
-                                    + $address->getBaseSubtotalWithDiscount() );
-
-      $this->setGrandTotal((float) $this->getGrandTotal()
-                                                  + $address->getGrandTotal());
-      $this->setBaseGrandTotal((float) $this->getBaseGrandTotal()
-                                              + $address->getBaseGrandTotal());
-    }
-
-    Mage::helper('sales')->checkQuoteAmount($this, $this->getGrandTotal());
-    Mage::helper('sales')->checkQuoteAmount($this, $this->getBaseGrandTotal());
-
-    $this->setItemsCount(0);
-    $this->setItemsQty(0);
-    $this->setVirtualItemsQty(0);
-
-    foreach ($this->getAllVisibleItems(true) as $item) {
-      if ($item->getParentItem())
-        continue;
-
-      $children = $item->getChildren();
-
-      if ($children && $item->isShipSeparately())
-        foreach ($children as $child)
-          if ($child->getProduct()->getIsVirtual())
-            $this->setVirtualItemsQty($this->getVirtualItemsQty()
-                                         + $child->getQty() * $item->getQty() );
-
-      if ($item->getProduct()->getIsVirtual())
-        $this->setVirtualItemsQty($this->getVirtualItemsQty() + $item->getQty());
-
-      $this->setItemsCount($this->getItemsCount() + 1);
-      $this->setItemsQty((float) $this->getItemsQty() + $item->getQty());
-    }
-
-    $this->setData('trigger_recollect', 0);
-    $this->_validateCouponCode();
-
-    Mage::dispatchEvent($this->_eventPrefix . '_collect_totals_after',
-                                            array($this->_eventObject=>$this) );
-
-    $this->setTotalsCollectedFlag(true);
+    //Save collection as item_collection in the quote.
+    //See getItemsCollection() method.
+    $this->setItemsCollection($items);
 
     return $this;
   }
 
   public function setIsActive ($active) {
+    if ($active || !$this->hasItemsCollection())
+      return parent::setIsActive($active);
+
     //Change quote active state only then it contains only approved items,
     //otherwise ignore it
 
-    if (!$active
-        && $this->getAllItemsCount() != count($this->getAllVisibleItems()))
-      return $this;
+    $approvedItems = $this->getItemsCollection();
 
-    return parent::setIsActive($active);
-  }
+    $this->setItemsCollection(null);
 
-  public function hasProductId ($productId) {
-    foreach ($this->getAllItems(true) as $item)
-      if ($item->getProductId() == $productId)
-        return true;
+    $allItemsNumber = count($this->getItemsCollection());
+    $approvedItemsNumber = count($approvedItems);
 
-    return false;
-  }
+    $this->setItemsCollection($approvedItems);
 
-  public function updateItem ($itemId, $buyRequest, $params = null) {
-    $item = $this->getItemById($itemId, true);
-
-    if (!$item)
-      Mage::throwException(Mage::helper('sales')
-                          ->__('Wrong quote item id to update configuration.'));
-
-    $productId = $item->getProduct()->getId();
-
-    //We need to create new clear product instance with same $productId
-    //to set new option values from $buyRequest
-    $product = Mage::getModel('catalog/product')
-                 ->setStoreId($this->getStore()->getId())
-                 ->load($productId);
-
-    if (!$params)
-      $params = new Varien_Object();
-    else if (is_array($params))
-      $params = new Varien_Object($params);
-
-    $params->setCurrentConfig($item->getBuyRequest());
-    $buyRequest = Mage::helper('catalog/product')
-                    ->addParamsToBuyRequest($buyRequest, $params);
-
-    $buyRequest->setResetCount(true);
-    $resultItem = $this->addProduct($product, $buyRequest);
-
-    if (is_string($resultItem))
-      Mage::throwException($resultItem);
-
-    if ($resultItem->getParentItem())
-      $resultItem = $resultItem->getParentItem();
-
-    if ($resultItem->getId() != $itemId) {
-      //Product configuration didn't stick to original quote item
-      //It either has same configuration as some other quote
-      //item's product or completely new configuration
-      $this->removeItem($itemId, true);
-
-      $items = $this->getAllItems(true);
-
-      foreach ($items as $item)
-        if (($item->getProductId() == $productId)
-            && ($item->getId() != $resultItem->getId()))
-          if ($resultItem->compare($item)) {
-            // Product configuration is same as in other quote item
-            $resultItem->setQty($resultItem->getQty() + $item->getQty());
-            $this->removeItem($item->getId(), true);
-
-            break;
-          }
-    } else
-      $resultItem->setQty($buyRequest->getQty());
-
-    return $resultItem;
+    return $allItemsNumber == $approvedItemsNumber
+             ? parent::setIsActive($active)
+               : $this;
   }
 }
 
