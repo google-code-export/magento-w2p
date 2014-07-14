b023f2e20e7db3a49562d23f7b7039ea415239c8
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@89e7eeb8-28a2-11de-b02e-9db90c89ab99>
2014-07-14 00:59:47 +0000

    Set URL for the next step in observer

--- app/code/community/ZetaPrints/WebToPrint/Helper/2step.php
+++ app/code/community/ZetaPrints/WebToPrint/Helper/2step.php
@@ -6,4 +6,50 @@ class ZetaPrints_WebToPrint_Helper_2step
   public function isPersonalisationStep () {
     return Mage::registry('webtoprint_is_personalisation_step');
   }
+
+  public function getNextStepRoute ($product) {
+    $data = array(
+      'route' => 'catalog/product/view',
+      'params' => array(
+        'id' => $product->getId(),
+        's' => $product->getUrlKey(),
+        '_use_rewrite' => true,
+
+        //Add personalization parameter to URL
+        '_query' => array('personalization' => '1')
+      )
+    );
+
+    if ($product->getCategoryId() && !$product->getDoNotUseCategoryId())
+      $data['params']['category'] = $product->getCategoryId();
+
+    $request = Mage::app()->getRequest();
+
+    //Check if the product page was requested with reorder parameter
+    //then proxy the parameter to personalization step
+    if ($request->has('reorder'))
+      $data['_query']['reorder'] = $request->getParam('reorder');
+
+    //Check if the product page was requested with for-item parameter
+    //then proxy the parameter to personalization step and ignore last
+    //visited page (need it to distinguish cross-sell product and already
+    //personalized product)
+    if ($request->has('for-item'))
+      $data['_query']['for-item'] = $request->getParam('for-item');
+    else {
+      //Check that the product page was opened from cart page (need for
+      //automatic first preview update for cross-sell product)
+
+      $pos = strpos(
+        Mage::getSingleton('core/session')->getData('last_url'),
+        'checkout/cart'
+      );
+
+      if ($pos !== false)
+        //Send update-first-preview query parameter to personalization step
+        $data['_query']['update-first-preview'] = 1;
+    }
+
+    return $data;
+  }
 }
